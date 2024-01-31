35eb962c49854486bf179a1a72448def2dedb6f3
sujeet kumar <ksujeetkumar7678@gmail.con>
2024-01-31 20:36:18 +0530

    Update scroll behavior and scrollbar styles

--- components/sheared/AllAnswers.tsx
+++ components/sheared/AllAnswers.tsx
@@ -38,53 +38,57 @@ const AllAnswers = async ({
       </div>
 
       <div>
-        {result?.answers.map((answer) => (
-          <article
-            key={answer?._id}
-            className='light-border border-b py-10'
-          >
-            <div className='flex items-center justify-between'>
+        {result?.answers.map((answer) => {
+          const ansId = JSON.stringify(answer?._id);
+          return (
+            <article
+              key={answer?._id}
+              className='light-border border-b py-10'
+            >
               {/* span id -> to scroll */}
-              <div className='mb-8 flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2'>
-                <Link
-                  href={`/profile/${answer?.author?.clerkId}`}
-                  className='flex flex-1 items-start gap-1 sm:items-center'
-                >
-                  <Image
-                    src={answer.author.picture}
-                    width={18}
-                    height={18}
-                    alt='profile'
-                    className='rounded-full object-cover max-sm:mt-0.5'
-                  />
-                  <div className='flex flex-col sm:flex-row sm:items-center'>
-                    <p className='body-semibold text-dark300_light700'>
-                      {answer?.author?.name}
-                    </p>
-                    <p className='small-regular text-light400_light500 mt-0.5 line-clamp-1'>
-                      &nbsp; • &nbsp;
-                      <span className='max-sm:hidden '>
-                        answered &nbsp; {getTimeStamp(answer?.createdAt)}
-                      </span>
-                    </p>
+              <span id={ansId.replaceAll('"', "")}></span>
+              <div className='flex items-center justify-between'>
+                <div className='mb-8 flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2'>
+                  <Link
+                    href={`/profile/${answer?.author?.clerkId}`}
+                    className='flex flex-1 items-start gap-1 sm:items-center'
+                  >
+                    <Image
+                      src={answer.author.picture}
+                      width={18}
+                      height={18}
+                      alt='profile'
+                      className='rounded-full object-cover max-sm:mt-0.5'
+                    />
+                    <div className='flex flex-col sm:flex-row sm:items-center'>
+                      <p className='body-semibold text-dark300_light700'>
+                        {answer?.author?.name}
+                      </p>
+                      <p className='small-regular text-light400_light500 mt-0.5 line-clamp-1'>
+                        &nbsp; • &nbsp;
+                        <span className='max-sm:hidden '>
+                          answered &nbsp; {getTimeStamp(answer?.createdAt)}
+                        </span>
+                      </p>
+                    </div>
+                  </Link>
+                  <div className='flex justify-end'>
+                    <Votes
+                      type='answer'
+                      itemId={JSON.stringify(answer._id)}
+                      userId={JSON.stringify(userId)}
+                      upvotes={answer?.upvotes?.length}
+                      hasupVoted={answer?.upvotes?.includes(userId)}
+                      downvotes={answer?.downvotes?.length}
+                      hasdownVoted={answer?.downvotes?.includes(userId)}
+                    />
                   </div>
-                </Link>
-                <div className='flex justify-end'>
-                  <Votes
-                    type='answer'
-                    itemId={JSON.stringify(answer._id)}
-                    userId={JSON.stringify(userId)}
-                    upvotes={answer?.upvotes?.length}
-                    hasupVoted={answer?.upvotes?.includes(userId)}
-                    downvotes={answer?.downvotes?.length}
-                    hasdownVoted={answer?.downvotes?.includes(userId)}
-                  />
                 </div>
               </div>
-            </div>
-            <ParseHTML data={answer.content} />
-          </article>
-        ))}
+              <ParseHTML data={answer.content} />
+            </article>
+          );
+        })}
       </div>
     </div>
   );
